dae5fc5c304dc28167699f33b42e097a51b7a135
unknown <nagalakshmisank@gmail.com>
2022-11-26 17:06:13 +0530

    paragraph updated

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -8,7 +8,7 @@ const AboutMe = () => {
     <div className='info'>
       <h2>Welcome to our website!</h2>
       <p>We are excited to offer you a wide range of products and services. Our goal is to provide you with the best possible experience.</p>
-      <p>Pleae find my list of tasks in sidemenu.</p>
+      <p>Pleae find my list of tasks in sidemenu.You can click and refer</p>
       <p>Please take a look around and let us know if you have any questions.</p>
   </div>
     </div>
